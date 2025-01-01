fe5c0576d68f86365120dcdbccd035a7758c97c8
sonakshimathur07 <sonakshimathur117@gmail.com>
2025-01-01 21:30:49 +0530

    messege

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATD-KD02Aq5_qzVMvtMl-YhLlih-_kds8",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "netflix-f8f8b.firebaseapp.com",
   projectId: "netflix-f8f8b",
   storageBucket: "netflix-f8f8b.firebasestorage.app",
